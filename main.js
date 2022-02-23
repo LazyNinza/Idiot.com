6b100db53ad539b0a4d773e792f21412b40d3cda
LazyNinza <100315802+LazyNinza@users.noreply.github.com>
2022-02-23 12:36:15 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// var btn = document.getElementById('startBTN').onclick = function(){
+//     var bd = document.getElementById('bd');
+//     bd.style.backgroundColor = 'white';
+//     var parent = btn.p
+// }
+// function removeNODE(){
+//     // const btn = document.getElementById('startBTN');
+//     // const parent = btn.parentNode;
+//     // parent.removeChild(btn);
+//     // console.log('something');
+//     // var bd = document.getElementById('bd');
+//     // bd.style.backgroundColor = 'black';
+//     // var h4Pt1 = document.getElementById('myH4');
+//     // const parent2 = h4Pt1.parentNode;
+//     // parent2.removeChild(h4Pt1);
+//     // var h4Pt2 = document.getElementById('myH4Pt2');
+// };
